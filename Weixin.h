ea88a690c736f912b458feb97d17049abcf0eb1d
zyq98904 <812282381@qq.com>
2017-06-15 10:10:19 +0800

    90879

--- Weixin.h
+++ Weixin.h
@@ -14,7 +14,32 @@ class Tencent_weixin{
 			id=i;
 			name=n;
 		}
+		void Save();//----------------保存信息到文件 
+		const void put();//-----------输出个人信息 
+		const void jiemian();//-------登陆界面 
+		void Haoyoujiemian();//-------调用List1 
+		void load();//------------------读取好友列表 
+		void save_haoyou();//---------保存好友到文件 
+		void save_weiqun();//---------保存群信息到文件 
+		void guanlianQQ();//----------关联QQ 
+		void deletelink();//----------解除关联
+		void linkfriend_put();//----------查找关联账号好友
+		void linkQ_put();//---------------查找关联账号信息
+		void load_weiqun();//-------------创建读取微信信息文件
+		void weiqun_Apply();//------------申请微信群
+		void weiqun_put();//--------------输入加入群的信息
+		void weiqun_delete();//-----------退出群
+		void weiqun_manage();//-----------管理群
 	protected:
+		string id;//----------------------号码 
+		string name;//--------------------名字 
+		Date birthday;//------------------生日 
+		Date tyear;//---------------------申请时间 
+		Address where;//------------------地址 
+		string key;//---------------------密码 
+		List1 fried;//--------------------weixin好友向量 
+		List2 weiqun;//-------------------weixin群存储群向量 
+		string Beizhu;//------------------好友备注 
 };
 #endif
 
